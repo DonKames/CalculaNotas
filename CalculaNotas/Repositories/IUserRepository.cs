95d1d50a9965f66c0b05199e7b5ad42dca428395
Camilo Santander <camilo.santander1991@gmail.com>
2023-06-18 23:10:07 -0400

    test

---
+++ CalculaNotas/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculaNotas.Models;
+
+namespace CalculaNotas.Repositories
+{
+    internal interface IUserRepository
+    {
+        User GetUserById(int id);
+        // Otros métodos necesarios para trabajar con los usuarios
+    }
+}
