2694e26f9fe91a39ef82b69a3aa9b30ff9af41a5
Camilo Santander <camilo.santander1991@gmail.com>
2023-06-23 00:17:11 -0400

    working

---
+++ CalculaNotas/Repositories/CourseRepository.cs
@@ -0,0 +1,44 @@
+﻿using CalculaNotas.Data;
+using CalculaNotas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaNotas.Repositories
+{
+    internal class CourseRepository : ICourseRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public Task<Course> AddCourse(Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCourse(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Course>> GetAllCourses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Course>> GetAllCoursesBySemesterId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Course> GetCourseById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCourse(Course course)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CalculaNotas/Repositories/ICourseRepository.cs
+++ CalculaNotas/Repositories/ICourseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CalculaNotas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace CalculaNotas.Repositories
 {
     internal interface ICourseRepository
     {
+        Task<Course> GetCourseById(int id);
+        Task<List<Course>> GetAllCourses();
+        Task<List<Course>> GetAllCoursesBySemesterId(int id);
+        Task<Course> AddCourse(Course course);
+        Task UpdateCourse(Course course);
+        Task DeleteCourse(int id);
     }
 }
